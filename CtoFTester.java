3a721bbbff3ff92d8ca503d534a9aeba1d390f08
NicoleLiu123 <nliu30@stuy.edu>
2021-09-23 21:45:39 -0400

    Answered Questions Before Coding

---
+++ CtoFTester.java
@@ -0,0 +1,7 @@
+//Nicole Liu
+//Period 9
+//The function celsiusToFahrenheit() should have the parameter celsius which will be replaced by the temperature
+//in celsius. This parameter will be a double since temperatures can be decimals. The function fahrenheitToCelsius()
+//should have the parameter fahrenheit which will be replaced by the temperature in fahrenheit. This parameter will also
+//be a double. The function celsiusToFahrenheit() should return the Fahrenheit conversion of the celsius argument.
+// The function fahrenheitToCelsius() should return the Celsius conversion of the fahrenheit argument.
